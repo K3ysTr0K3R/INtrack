97a4a52aab21659530dfb13d6b2ad05ce7dc5482
k3ystr0k3r <70909693+K3ysTr0K3R@users.noreply.github.com>
2024-08-30 08:18:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 import random
 import socket
@@ -9,6 +11,7 @@ from lib.gargoyle_scanner import check_gargoyle
 from lib.gpon_scanner import check_gpon
 from lib.webcamxp_scanner import check_webcamxp
 from lib.vscode_sftp_worm import crawl_vscode_sftp
+from lib.CVE_2017_7921 import check_CVE_2017_7921
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -30,6 +33,11 @@ def process_ip(ip, args):
             crawl_vscode_sftp(ip, args.p)
             return ip
 
+    if args.vuln:
+        if args.vuln == 'CVE-2017-7921':
+            check_CVE_2017_7921(ip, args.p)
+            return ip
+
     if args.instance:
         if args.instance == 'wordpress' and check_wordpress(ip, args.p):
             return ip
@@ -49,8 +57,10 @@ def main():
     parser.add_argument("-n", type=int, required=True, help="Number of targets to find.")
     parser.add_argument("-p", type=int, default=None, help="Port to check.")
     parser.add_argument("-t", type=int, default=1, help="Number of threads to use.")
+    parser.add_argument("-o", type=str, help="Store results into a file.")
     parser.add_argument("-instance", type=str, help="Type of instance to check (e.g., 'wordpress', 'gargoyle', 'gpon').")
     parser.add_argument("-worm", type=str, help="Enable special script execution with a specified type (e.g., 'vscode-sftp').")
+    parser.add_argument("-vuln", type=str, help="Enable vuln script execution with a specified type (e.g., CVE-2017-7921)")
 
     args = parser.parse_args()
 
@@ -61,9 +71,19 @@ def main():
             ip_addresses = [generate_ip() for _ in range(args.t * 10)]
             with concurrent.futures.ThreadPoolExecutor(max_workers=args.t) as executor:
                 results = list(executor.map(lambda ip: process_ip(ip, args), ip_addresses))
-            found_targets.extend(filter(None, results))
+
+            new_targets = list(filter(None, results))
+            found_targets.extend(new_targets)
+
+            if args.o:
+                with open(args.o, 'a') as file:
+                    for target in new_targets:
+                        file.write(f"{target}\n")
+
             instance_bar(len(ip_addresses))
+
     print(f"\n[*] Total targets found: {len(found_targets)}")
+    print("")
     for targ in found_targets[:args.n]:
         print(targ)
 
