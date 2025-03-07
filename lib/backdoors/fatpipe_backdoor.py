e33e065bcc047a645307c600e6fbd5e887346c65
k3ystr0k3r <70909693+K3ysTr0K3R@users.noreply.github.com>
2025-03-07 17:12:53 -0500

    Update fatpipe_backdoor.py

--- lib/backdoors/fatpipe_backdoor.py
+++ lib/backdoors/fatpipe_backdoor.py
@@ -18,7 +18,7 @@ def fatpipe_backdoor(ip, ports=None, timeout=10):
                 data = "loginParams=%7B%22username%22%3A%22cmuser%22%2C%22password%22%3A%22%22%2C%22authType%22%3A0%7D"
                 response = requests.post(url, data=data, verify=False, timeout=timeout, headers=headers)
                 if '"loginRes":"success"' in response.text and '"activeUserName":"cmuser"' in response.text:
-                    print_colour(f"FatPipe backdoor detected: {url}")
+                    print_colour(f"[+] FatPipe backdoor detected: {url}")
                     return True
             except requests.RequestException:
                 continue
